e60a9c99ad8dee97c59dc6aa26fd68204eb6af69
James Constable <jwdconstable@gmail.com>
2021-02-11 15:55:55 +1100

    Add solution for 2.03 and 2.04

--- p2_arithmetic.pl
+++ p2_arithmetic.pl
@@ -21,6 +21,7 @@ prime_factors(N, Fs) :-
   N > 1, range(2, N, R), prime_sieve(R, Ps), prime_factors(N, Ps, Fs).
 
 prime_factors(_, [], []).
+prime_factors(1, _, []) :- !.
 prime_factors(N, [P|Ps], Fs) :- N mod P =\= 0, !, prime_factors(N, Ps, Fs).
 prime_factors(N, [P|Ps], [P|Fs]) :- N2 is N / P, prime_factors(N2, [P|Ps], Fs).
 
@@ -33,3 +34,29 @@ filter_multiples(N, [X|Xs], [X|T]) :- filter_multiples(N, Xs, T).
 
 range(N, N, [N]).
 range(A, B, [A|R]) :- A \= B, A_ is A + sign(B-A), range(A_, B, R).
+
+
+% 2.03 (**) Determine the prime factors of a given positive integer (2).
+% Construct a list containing the prime factors and their multiplicity.
+
+% Example:
+% ?- prime_factors_mult(315, L).
+% L = [[3, 2], [5, 1], [7, 1]]
+
+prime_factors_mult(N, FMs) :- prime_factors(N, Fs), group_count(Fs, FMs).
+
+group_count([], []).
+group_count([X], [[X, 1]]).
+group_count([X|Xs], [[X, N_]|E]) :- group_count(Xs, [[X, N]|E]), N_ is N + 1.
+group_count([X|Xs], [[X, 1], [Y, N]|E]) :- group_count(Xs, [[Y, N]|E]), X \= Y.
+
+
+% 2.04 (*) A list of prime numbers.
+% Given a range of integer by its lower and upper limit, construct a list of all
+% prime numbers in that range.
+
+primes_in_range(L, U, RPs) :- range(2, U, R), prime_sieve(R, Ps),
+  drop_while_less_than(L, Ps, RPs).
+
+drop_while_less_than(N, [X|Xs], T) :- X < N, !, drop_while_less_than(N, Xs, T).
+drop_while_less_than(_, Xs, Xs).
