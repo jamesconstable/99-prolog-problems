2620573d470d2626f10291c24084cfba3c7c4dec
James Constable <jwdconstable@gmail.com>
2021-02-12 23:52:21 +1100

    Add solution for 3.01

---
+++ p3_logic_and_codes.pl
@@ -0,0 +1,43 @@
+% 3.01 (**) Truth tables for logical expressions.
+% Define predicates and/2, or/2, nand/2, nor/2, xor/2, impl/2 and equ/2 (for
+% logical equivalence) which succeed or fail according to the result of their
+% respective operations; e.g. and(A, B) will succeed, if and only if both A and
+% B succeed. Note that A and B can be Prolog goals (not only the constants true
+% and fail).
+
+% A logical expression in two variables can then be written in prefix notation,
+% as in the following example: and(or(A, B), nand(A, B)).
+
+% Now, write a predicate table/3 which prints the truth table of a given logical 
+% expression in two variables.
+
+% Example:
+% ?- table(A, B, and(A, or(A, B))).
+% true  true  true
+% true  false true
+% false true  false
+% false false false
+
+and(A, B) :- A, B.
+
+or(A, _) :- A.
+or(_, B) :- B.
+
+nand(A, B) :- not(and(A, B)).
+
+nor(A, B) :- not(or(A, B)).
+
+xor(A, B) :- and(or(A, B), not(and(A, B))) .
+
+impl(A, B) :- or(not(A), B).
+
+equ(A, B) :- not(xor(A, B)).
+
+table(A, B, Pred) :- bind(A), bind(B), write_row(A, B, Pred), fail.
+table(_, _, _).
+
+bind(true).
+bind(false).
+
+write_row(A, B, P) :- P,   writef("%t\t%t\t%t", [A, B, true]), nl.
+write_row(A, B, P) :- \+P, writef("%t\t%t\t%t", [A, B, false]), nl.
