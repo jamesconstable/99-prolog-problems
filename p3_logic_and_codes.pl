d9ca0f09c25dd099a26b20d92eca96ccb500ae88
James Constable <jwdconstable@gmail.com>
2021-02-13 16:26:19 +1100

    Add solution for 3.05

--- p3_logic_and_codes.pl
+++ p3_logic_and_codes.pl
@@ -121,3 +121,65 @@ gray(N, C) :-
 
 prepend_all(_, [], []).
 prepend_all(S, [X|Xs], [Y|Ys]) :- atom_concat(S, X, Y), prepend_all(S, Xs, Ys).
+
+
+% 3.05 (***) Huffman code.
+% First of all, study a good book on discrete mathematics or algorithms for a
+% detailed description of Huffman codes, or consult Wikipedia.
+
+% We suppose a set of symbols with their frequencies, given as a list of fr(S,F)
+% terms. Example: [fr(a, 45), fr(b, 13), fr(c, 12), fr(d, 16), fr(e, 9),
+% fr(f, 5)]. Our objective is to construct a list hc(S, C) terms, where C is the
+% Huffman code word for the symbol S. In our example, the result could be
+% Hs = [hc(a, '0'), hc(b, '101'), hc(c, '100'), hc(d, '111'), hc(e, '1101'),
+% hc(f, '1100')]. The task shall be performed by the predicate huffman/2 defined
+% as follows:
+
+% huffman(Fs, Hs) :- Hs is the Huffman code table for the frequency table Fs
+
+huffman([], []).
+huffman([fr(H, _)], H) :- is_list(H).
+huffman([fr(S, _)], [hc(S, '0')]) :- \+ is_list(S).
+huffman(Fs, Hs) :-
+  min(Fs, A, Fs1),
+  min(Fs1, B, Fs2),
+  combine(A, B, C),
+  huffman([C|Fs2], Hs).
+
+min([A], A, []).
+min([SF|SFs], SF, [M|R]) :-
+  min(SFs, M, R),
+  SF = fr(_, F),
+  M = fr(_, Fm),
+  F =< Fm.
+min([SF|SFs], M, [SF|R]) :-
+  min(SFs, M, R),
+  SF = fr(_, F),
+  M = fr(_, Fm),
+  F > Fm.
+
+combine(fr(Sa, Fa), fr(Sb, Fb), fr([hc(Sa, '0'), hc(Sb, '1')], Fc)) :-
+  \+ is_list(Sa),
+  \+ is_list(Sb),
+  Fc is Fa + Fb.
+combine(fr(Sa, Fa), fr(Sb, Fb), fr([hc(Sa, '0')|HCs], Fc)) :-
+  \+ is_list(Sa),
+  is_list(Sb),
+  prepend_all_codes('1', Sb, HCs),
+  Fc is Fa + Fb.
+combine(fr(Sa, Fa), fr(Sb, Fb), fr([hc(Sb, '0')|HCs], Fc)) :-
+  is_list(Sa),
+  \+ is_list(Sb),
+  prepend_all_codes('1', Sa, HCs),
+  Fc is Fa + Fb.
+combine(fr(Sa, Fa), fr(Sb, Fb), fr(HCs, Fc)) :-
+  is_list(Sa),
+  is_list(Sb),
+  prepend_all_codes('0', Sa, HCs1),
+  prepend_all_codes('1', Sb, HCs2),
+  append(HCs1, HCs2, HCs),
+  Fc is Fa + Fb.
+
+prepend_all_codes(_, [], []).
+prepend_all_codes(P, [hc(S, C)|Xs], [hc(S, C_)|Ys]) :-
+  atom_concat(P, C, C_), prepend_all_codes(P, Xs, Ys).
