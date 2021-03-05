dd6080238f9921ce607ac6246f4cc260a5b4133d
James Constable <jwdconstable@gmail.com>
2021-03-06 04:44:05 +1100

    Add solution for 7.03

--- p7_miscellaneous.pl
+++ p7_miscellaneous.pl
@@ -1,3 +1,5 @@
+:- ensure_loaded(p6_graphs).
+
 % 7.01 (**) Eight queens problem.
 % This is a classical problem in computer science. The objective is to place
 % eight queens on a chessboard so that no two queens are attacking each other;
@@ -58,9 +60,62 @@ jump(N, X/Y, U/V) :-
 jump_delta(X/Y) :- member(X, [-2, 2]), member(Y, [-1, 1]).
 jump_delta(X/Y) :- member(X, [-1, 1]), member(Y, [-2, 2]).
 
-memberchk_dl(M, [H|_]) :- nonvar(H), M=H, !.
-memberchk_dl(M, [_|L]) :- nonvar(L), memberchk_dl(M, L).
 
+% 7.03 (***) Von Koch's conjecture.
+% Several years ago I met a mathematician who was intrigued by a problem for
+% which he didn't know a solution. His name was Von Koch, and I don't know
+% whether the problem has been solved since. The problem goes like this:
+% given a tree with N nodes (and hence N-1 edges), find a way to enumerate the
+% nodes from 1 to N and, accordingly the edges from 1 to N-1, in such a way that
+% for each edge k the difference of its node numbers equals to K. The conjecture
+% is that this is always possible.
+
+% For small trees the problem is easy to solve by hand. However, for larger
+% trees, and 14 is already very large, it is extremely difficult to find a
+% solution. And remember, we don't know for sure whether there is always a
+% solution!
+
+% Write a predicate that calculates a numbering scheme for a given tree. What is
+% the solution for the larger tree pictured at:
+% https://sites.google.com/site/prologsite/prolog-problems/7/p92b.gif
+
+% One solution (turns out there are many):
+% [a=1, b=2, c=12, d=3, e=4, f=5, g=11, h=13, i=14, k=8, m=6, n=7, p=9, q=10]
+
+van_koch(G, Enum) :-
+  graph_term(G, Ns, Es, _),
+  length(Ns, N),
+  N1 is N-1,
+  numlist(1, N, NodeValues),
+  numlist(1, N1, EdgeValues),
+  van_koch(Ns, Es, Enum, NodeValues, EdgeValues).
+
+van_koch([], [], Enum, [], []) :- close_dl(Enum).
+van_koch([N|Ns], Es, Enum, AvailableNodes, AvailableEdges) :-
+  select(N1, AvailableNodes, AvailableNodes1),
+  map_and_check_edges(N, N1, Es, Es1, AvailableEdges, AvailableEdges1),
+  memberchk(N=N1, Enum),
+  van_koch(Ns, Es1, Enum, AvailableNodes1, AvailableEdges1).
+
+map_and_check_edges(N, N1, Es, Unmapped, AvailableEs, AvailableEs1) :-
+  maplist({N,N1}/[E,E1]>>map_edge(N, N1, E, E1), Es, Es1),
+  partition(fully_mapped, Es1, Mapped, Unmapped),
+  foldl([m(A, B, _), Es, Es1]>>(
+      Diff is abs(A-B),
+      memberchk(Diff, Es),
+      ord_subtract(Es, [Diff], Es1)),
+    Mapped, AvailableEs, AvailableEs1).
+
+% m terms represent partially or fully mapped edges. They have one of two
+% structures:
+%   m(A, nil, B) -> Partially mapped: A is mapped; B is unmapped
+%   m(A, B, nil) -> Fully mapped
+map_edge(N, N1, E, m(N1, nil, B)) :- edge_terms(E, _, _, [N, B]), !.
+map_edge(N, N1, E, m(N1, nil, A)) :- edge_terms(E, _, _, [A, N]), !.
+map_edge(N, N1, m(A, nil, N), m(A, N1, nil)) :- !.
+map_edge(_, _, E, E).
+
+fully_mapped(m(_, _, nil)).
 
 % 7.05 (**) English number words.
 % On financial documents, like cheques, numbers must sometimes be written in
